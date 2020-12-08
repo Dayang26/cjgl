48796eb9e8759c14a8b0f671be7014e25734619f
Dayang <hsyy26@qq.com>
2020-12-08 12:55:45 +0800

    12312

---
+++ src/main/java/com/snow/cjgl/CjglApplication.java
@@ -0,0 +1,13 @@
+package com.snow.cjgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CjglApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CjglApplication.class, args);
+    }
+
+}
